64a8445460a53385d417b5871bb8fe922e296631
Vladyslav Mihun <mihunvlad@gmail.com>
2023-01-13 18:38:20 +0200

    Added: html stubs, s3-website terraform configuration.

---
+++ README.md
@@ -0,0 +1,1 @@
+An example of terraform configuration for static aws s3 website.
---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_object" "website" {
+  for_each = fileset(path.root, "*.html")
+
+  bucket       = aws_s3_bucket.website.bucket
+  key          = each.value
+  source       = "${path.root}/${each.value}"
+  etag         = filemd5("${path.root}/${each.value}")
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.allow_public_read.json
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.website.bucket
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "not_found.html"
+  }
+}
+
+data "aws_iam_policy_document" "allow_public_read" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+output "website" {
+  value     = "http://${var.bucket_name}.s3-website.${aws_s3_bucket.website.region}.amazonaws.com/"
+  sensitive = false
+}
